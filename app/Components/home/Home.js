388399b225069993d4b7edf0a2c9fc183e9c80ea
sakibpro2003 <sakibprodhan2003@gmail.com>
2025-06-09 18:25:51 +0600

    first commit

---
+++ app/Components/home/Home.js
@@ -0,0 +1,40 @@
+import "./Home.css";
+import HeaderSocials from "./HeaderSocials";
+import ScrollDown from "./ScrollDown";
+import Shapes from "./Shapes";
+import PortfolioModal from "../modal/PortfolioModal";
+import Image from "next/image";
+
+const Home = () => {
+  return (
+    <>
+      <div className="fixed left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 z-[99999] w-80">
+        <PortfolioModal />
+      </div>
+      <section className="home container" id="home">
+        <div className="intro">
+          <Image
+            src="https://i.ibb.co/zS38BWV/9434621.png"
+            height={145}
+            width={160}
+            alt=""
+            className="home__img"
+          />
+          <h1 className="home__name">Chetan Kesare</h1>
+          <span className="home__education">I am a Full-Stack UX Designer</span>
+
+          <HeaderSocials />
+
+          <a href="#contact" className="btn">
+            Hire Me
+          </a>
+          <ScrollDown />
+        </div>
+
+        <Shapes />
+      </section>
+    </>
+  );
+};
+
+export default Home;
---
+++ app/Components/resume/Data.js
@@ -0,0 +1,45 @@
+// Config Driven UI
+
+const Data =[
+    {
+        id:1,
+        category:"education",
+        icon:"icon-graduation",
+        year: "2017-2018",
+        title:"Class 10 CBSE",
+        desc:"70%",
+    },
+    {
+        id:2,
+        category:"education",
+        icon:"icon-graduation",
+        year: "2018-2021",
+        title:"Diploma In Computer Engineering",
+        desc:"91.20%",
+    },
+    {
+        id:3,
+        category:"education",
+        icon:"icon-graduation",
+        year: "2021-2024",
+        title:"B.E In Computer Engineering",
+        desc:"8.4 CGPA",
+    },
+    {
+        id:4,
+        category:"experience",
+        icon:"icon-briefcase",
+        year: "July-Augest-2023",
+        title:"CodeSoft",
+        desc:"UI/UX Design Intern at CodeSoft.",
+    },
+    {
+        id:4,
+        category:"experience",
+        icon:"icon-briefcase",
+        year: "June-July-2020",
+        title:"Domain Computer Education",
+        desc:"CISCO Router Switch Configuration and AWS Cloud Essentail.",
+    },
+];
+export default Data
